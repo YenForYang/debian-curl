3635d9debf0e49003b4481b876a7f44ae67bf3bd
Richard <richard@yenforyang.com>
2019-01-06 04:55:37 +0100

    first commit

---
+++ curl/debian/build/tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'ccache /usr/bin/cpp.intel -Wdate-time -D_FORTIFY_SOURCE=2 -we140,147,165,266 -wd279,981,1469 -isystem /usr/include/mit-krb5';
+
+1;
